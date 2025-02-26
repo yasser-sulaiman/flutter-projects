c08a4281ae2c3072fbfcf805f9e861cf60917e9d
yasser-sulaiman <yassersulaiman.is@gmail.com>
2025-02-26 17:57:44 +0100

    Implement local database for storing user places and update PlacesScreen to load data asynchronously

--- favorite_places/lib/providers/user_places.dart
+++ favorite_places/lib/providers/user_places.dart
@@ -3,14 +3,76 @@ import 'dart:io';
 import 'package:favorite_places/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
+
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
+
+Future<Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  return sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)',
+      );
+    },
+    version: 1,
+  );
+}
+
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super([]);
 
-  void addPlace(String title, File image, PlaceLocation location) {
-    state = [Place(title: title, image: image, location: location), ...state];
+  Future<void> loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+    final loadedPlaces = data
+        .map(
+          (place) => Place(
+            id: place['id'] as String,
+            title: place['title'] as String,
+            image: File(place['image'] as String),
+            location: PlaceLocation(
+              latitude: place['lat'] as double,
+              longitude: place['lng'] as double,
+              address: place['address'] as String,
+            ),
+          ),
+        )
+        .toList();
+
+    state = loadedPlaces;
+  }
+
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(image.path);
+    final savedImage = await image.copy('${appDir.path}/$fileName');
+
+    final newPlace = Place(title: title, image: savedImage, location: location);
+
+    final db = await _getDatabase();
+
+    db.insert(
+      'user_places',
+      {
+        'id': newPlace.id,
+        'title': newPlace.title,
+        'image': newPlace.image.path,
+        'lat': newPlace.location.latitude,
+        'lng': newPlace.location.longitude,
+        'address': newPlace.location.address,
+      },
+      conflictAlgorithm: sql.ConflictAlgorithm.replace,
+    );
+
+    state = [newPlace, ...state];
   }
 }
 
-final userPlacesProvider = StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+final userPlacesProvider =
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
   (ref) => UserPlacesNotifier(),
 );
