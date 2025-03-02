6db6b1e0219b00efa22ef0791fe085f83b621484
yasser-sulaiman <yassersulaiman.is@gmail.com>
2025-03-02 17:39:21 +0100

    Refactor ChatScreen to support push notifications with Firebase Messaging

--- chat_app/lib/screens/chat.dart
+++ chat_app/lib/screens/chat.dart
@@ -2,10 +2,30 @@ import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  void setupPushNotification() async{
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    final token = await fcm.getToken();
+    print('Token: $token');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotification();
+  }
 
   @override
   Widget build(BuildContext context) {
